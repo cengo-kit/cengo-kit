4589325fa04d30d0858338dc336356046658b578
daghan.gunay <daghangunay@gmail.com>
2016-06-22 20:14:27 +0300

    javascript mimarisi eklendi

---
+++ app/scripts/app.js
@@ -0,0 +1,123 @@
+'use strict';
+var Gri = {
+  // Runtime da tum moduller buraya atanir.
+  modules: [],
+  _module: null,
+  valid: true,
+  _debug: Cookies.get('debug')
+};
+
+/*
+ Module icin ie kontrolu yapar.
+ */
+Gri.checkIEVersion = function () {
+  var _moduleIeVersion = this._module.ieVersion;
+  var moduleIeVersion = Number(ieV.replace('<', '').replace('>', ''));
+  var uA = navigator.userAgent;
+  var ieVersion = null;
+
+  if (uA.indexOf('MSIE 6') >= 0) {
+    ieVersion = 6;
+  }
+  if (uA.indexOf('MSIE 7') >= 0) {
+    ieVersion = 7;
+  }
+  if (document.documentMode) { // as of IE8
+    ieVersion = document.documentMode;
+  }
+
+
+  //Ie versiyon kontrolu yapilmis mi kontrolu
+  if (is.string(_moduleIeVersion)) {
+    this._module.ieVersion = null;
+  }
+
+  //Versiyon buyukse degilse valid keyini false eder
+  if (!(ieV.indexOf('>') && moduleIeVersion > ieVersion)) {
+    this.valid = false
+  }
+
+  //Versiyon kucukse degilse valid keyini false eder
+  if (!(ieV.indexOf('<') && moduleIeVersion < ieVersion)) {
+    this.valid = false
+  }
+
+  //Versiyon esit degilse valid keyini false eder
+  if (!(ieV.indexOf('=') && moduleIeVersion == ieVersion)) {
+    this.valid = false
+  }
+
+
+  return this;
+};
+
+/*
+ Module icin event atamasi yapar.
+ */
+Gri.setEvent = function () {
+  var $el = this._module.$el;
+  var container = this._module.container;
+  var state = this._module.state;
+  var fn = this._module.fn;
+
+  //State atanmis ise event olarak tanimliyoruz.
+  if (is.string(state)) {
+    if (is.string(container)) {
+      $(container).on(state, fn);
+    } else {
+      $($el).on(state, fn);
+    }
+  }
+  return this;
+};
+
+/*
+ Debug modu kontrolu yapar.
+ */
+Gri.debug = function (bool) {
+  if (is.boolean(bool)) {
+    this._debug = bool;
+    Cookies.set('debug', bool);
+  } else {
+    return this._debug;
+  }
+};
+
+/*
+ Runtimeda tum moduller bu methodu calistirir
+ */
+Gri.module = function (module) {
+  this.modules.push(module);
+};
+
+/*
+ Framework icin baslatici fonksiyondur.
+ */
+Gri.init = function () {
+  var gri = this;
+  //Tum modulleri document ready de calistirir.
+  $.each(this.modules, function (index, module) {
+    this._module = module;
+
+    //Gerekli oncelik siralariyla filitreleri calistiyoruz.
+    gri.checkIEVersion()
+      .setEvent()
+      .run();
+
+  });
+};
+
+/*
+ Modulu calistirir.
+ */
+Gri.run = function () {
+  var name = this._module.name;
+  var fn = this._module.fn;
+  fn();
+
+  if (this.debug()) {
+    KE.debug('Modul %c' + name + '%c yuklendi.');
+  }
+
+  return this;
+};
