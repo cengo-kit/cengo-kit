b3920d6ce5bc998b505b64e4d0bd40867d851884
daghan.gunay <daghangunay@gmail.com>
2016-09-26 17:26:17 +0300

    script : remove git submodule

--- cms.bat
+++ cms.bat
@@ -1,1 +1,1 @@
-echo "All" | xcopy "bower.json" "..\Website" && xcopy ".gitignore" "..\" && cd "..\" && git init && git add . && git commit -m "FIRST" && cd "Website" && bower install
+echo "All" | xcopy "bower.json" "..\Website" && xcopy ".gitignore" "..\" && del /F /S /Q /A .git && cd "..\" && git init && git add . && git commit -m "FIRST" && cd "Website" && bower install
