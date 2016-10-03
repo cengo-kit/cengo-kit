ef33160460c40995ffca9d968de7173ed5357ad8
daghan.gunay <daghangunay@gmail.com>
2016-10-03 17:18:29 +0300

    bat updated

--- cms.bat
+++ cms.bat
@@ -1,1 +1,29 @@
-xcopy /Y "bower.json" "..\Website" && xcopy /Y ".gitignore" "..\" && del /F /S /Q /A .git && cd "..\" && git init && git add . && git commit -m "FIRST" && cd "Website" && bower install
+xcopy /Y "bower.json" "..\Website"
+xcopy /Y ".gitignore" "..\"
+
+cd "..\"
+test&cls
+type sign.txt
+
+timeout 3 >nul
+
+git init
+
+git add Website\bin
+git commit -m "CMS Binaries"
+
+git add Website\App_Code
+git commit -m "App Code"
+
+git add Website
+git commit -m "CMS"
+
+git add HTML
+git commit -m "KIT"
+
+git add .
+git commit -m "Utils"
+
+timeout 3 >nul
+cd "Website"
+bower install
---
+++ cms/patch/Cengo/Cengo/Page.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cms13.Classes;
+using Cms13.Models;
+
+namespace Cengo
+{
+    public class Page : ViewPages 
+    {
+	    static CMS13Entities db = new CMS13Entities();
+	    private int PageID = 0;
+
+	    public Page(int PageID)
+	    {
+		    this.PageID = PageID;
+	    }
+
+	    public Page Parent()
+	    {
+		    var page = db.tblPages.FirstOrDefault(x => x.PageID == this.PageID);
+		    if (page != null)
+		    {
+			    this.PageID = page.SubPageID;
+			    return this;
+		    }
+		    else
+		    {
+			    throw new System.ArgumentException("Bu id ile bir page bulunamadi.");
+		    }
+	    }
+
+	    public ViewPages GetPage()
+	    {
+		    var page = db.sp_getHierachy_ViewPages(this.PageID).ToList()[0];
+			if (page != null)
+			{
+				return page;
+			}
+		    else
+		    {
+			    throw new System.ArgumentException("Bu id ile bir page bulunamadi.");
+		    }
+	    }
+
+		public void CopyValues<T>(T target, T source)
+		{
+			Type t = typeof(T);
+
+			var properties = t.GetProperties().Where(prop => prop.CanRead && prop.CanWrite);
+
+			foreach (var prop in properties)
+			{
+				var value = prop.GetValue(source, null);
+				if (value != null)
+					 prop.SetValue(target, value, null);
+			}
+		}
+
+    }
+}
