52276a3043c2b8049f1c80b8d69ea675d9aaafea
daghan.gunay <daghangunay@gmail.com>
2016-11-17 17:30:23 +0300

    hotfix

--- gulp-folder/tasks/build.js
+++ gulp-folder/tasks/build.js
@@ -2,3 +2,6 @@ gulp.task('build', ['lint','views', 'html', 'images', 'fonts', 'extras'], () => 
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
+gulp.task('build:cms', ['info:cms', 'scripts:cms','images:cms', 'inject:js:cms', 'views', 'iconfont', 'styles:cms', 'fonts:cms'], () => {
+  gulp.info = "cms";
+});
