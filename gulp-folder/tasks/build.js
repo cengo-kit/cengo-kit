71fa5250989c133d04c78b31f1b4acc066b965cc
kutbettin <info@kutbettin.com>
2016-11-24 16:18:22 +0200

    gulp build styles bug çözüldü.

--- gulp-folder/tasks/build.js
+++ gulp-folder/tasks/build.js
@@ -2,6 +2,7 @@ gulp.task('build', ['lint','views', 'html', 'images', 'fonts', 'extras'], () => 
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
-gulp.task('build:cms', ['info:cms', 'scripts:cms','images:cms', 'inject:js:cms', 'views', 'iconfont', 'styles:cms', 'fonts:cms'], () => {
+gulp.task('build:cms', ['info:cms', 'scripts:cms','images:cms', 'inject:js:cms', 'views', 'iconfont', 'fonts:cms'], () => {
+    gulp.start('styles:cms');
   gulp.info = "cms";
 });
