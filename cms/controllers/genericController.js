6336f6a2b7a51b30b9f35d697bbeea1203445e8c
daghan.gunay <Dghn12-12>
2017-04-03 15:19:26 +0300

    generated contets removed from vcs

--- cms/controllers/genericController.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(cms){
-  return {
-    render: require('./renderController').bind(cms)
-  };
-};
-
--- cms/controllers/renderController.js
+++
@@ -1,7 +0,0 @@
-module.exports  = function render(html, type){
-  let keys = this[type];
-  let gulpInfo = (this.gulpInfo) ? "html" : "cms";
-  for (var i = 0; i < keys.length; i++) {
-    html = html.replace(new RegExp(this[type][i]['key'], 'g'), this[key][i][gulpInfo])
-  }
-};
