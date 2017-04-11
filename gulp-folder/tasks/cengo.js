9b2284dcd62a8e0fce4dd95ee6ba23ef6608a882
kutbettin.yavuz <kutbettin.yavuz@gricreative.com>
2017-04-11 11:52:47 +0300

    gulp çoklu module oluşturma eklendi.

--- gulp-folder/tasks/cengo.js
+++ gulp-folder/tasks/cengo.js
@@ -12,6 +12,28 @@ gulp.task('cengo', ()=> {
       .pipe(gulp.dest('app/modules/' + name + "/"));
   }
 
+  if (type == "--modules" || type == "-ms") {
+    // Confluence ten documanı http://www.regexr.com/ adresine kopyalayıp modules:([\w ,-]+) kodu yapıştırın sonra sol taraftan flags tıklayıp ignore case(i) ve global(g) ı aktif edin.
+    // aşağıdaki tolls bölümünden list tıklayıp $1\n kodunu yapıştırın. çıkan çıktıyı bir txt dosyasına yapıştırıp "gulp cengo -ms name.txt" komutunu çalıştırın.
+    let page = fs.readFileSync('./' + name, 'utf8');
+    if (page) {
+      let modulesLines = page.split('\n');
+      modulesLines.forEach((line) => {
+        const modules = line.split(',');
+        modules.forEach((moduleName) => {
+          if (moduleName) {
+            moduleName = moduleName.replace(/[ \s]+/g, '');
+            gulp.src(
+              'cms/templates/module/**/*'
+            )
+              .pipe($.replace(/{{module_name}}/g, moduleName))
+              .pipe($.replaceName(/template/g, moduleName))
+              .pipe(gulp.dest('app/modules/' + moduleName + "/"));
+          }
+        });
+      });
+    }
+  }
   if (type == "--page" || type == "-p") {
     gulp.src(
       'cms/templates/page.jade'
